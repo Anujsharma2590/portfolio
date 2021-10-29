d418c02b3c5c9af0531e9367c7af5fea7485476c
Anuj sharma <aanuj.sharma2590@gmail.com>
2021-10-29 18:44:23 +0530

    file added

--- src/components/data/about_data.js
+++ src/components/data/about_data.js
@@ -1,5 +1,5 @@
 import api from "../../assets/icons/api.svg";
-import backend from "../../assets/icons/backend.svg";
+
 import algo from "../../assets/icons/algo.svg";
 import computer from "../../assets/icons/computer.svg";
 import repair from "../../assets/icons/repair.svg";
