c30b99b6d64304aaa9bde30a24a0ce4be9c0ffa1
Anuj sharma <aanuj.sharma2590@gmail.com>
2021-10-29 09:24:15 +0530

    demo portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import skills from './data/about_data'
+
+import Skillcard from "./Skillcard";
+
+
+
+const About = () => {
+  return (
+    <div className="about">
+      <h6 className="about__intro">
+        I describe myself as someone who's persistant, a quick learner and loves
+        problem solving by using simple and scalable solutions.
+      </h6>
+      <div className="container about__container">
+        <h6 className="about__heading">What I offer</h6>
+        <div className="row">
+          {skills.map((skill) => (
+            <Skillcard skill={skill} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
